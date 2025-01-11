9974d1990b2cf041ae49a73934855bc9a3a86c41
Henrique Silva <henriquersilva.al@gmail.com>
2025-01-11 19:03:55 -0300

    feat: migration de locações adicionada

--- database/migrations/2025_01_11_190721_create_rentals_table.php
+++ database/migrations/2025_01_11_190721_create_rentals_table.php
@@ -13,12 +13,30 @@ return new class extends Migration
     {
         Schema::create('rentals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('vehicle_id')->constrained()->onDelete('cascade');
+            $table->string('landlord_name');
+            $table->string('landlord_cpf');
+            $table->string('driver_license_number');
+            $table->date('driver_license_issue_date');
+            $table->date('birth_date');
+            $table->string('phone_1');
+            $table->string('phone_2')->nullable();
+            $table->string('mother_name');
+            $table->string('father_name')->nullable();
             $table->date('start_date');
             $table->date('end_date');
-            $table->decimal('total_price', 10, 2);
+            $table->decimal('cost', 10, 2);
+            $table->decimal('deposit', 10, 2);
+            $table->string('zip_code');
+            $table->string('state');
+            $table->string('city');
+            $table->string('neighborhood');
+            $table->string('street');
+            $table->string('house_number')->nullable();
+            $table->string('complement')->nullable();
+            $table->string('photo');
+            $table->foreignId('vehicle_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
