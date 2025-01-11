4f3d4dd61f6080bfba3ad5a91592e62e3a7d84be
Henrique Silva <henriquersilva.al@gmail.com>
2025-01-11 19:06:06 -0300

    feat: model locação adicionado

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -4,17 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rental extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['user_id', 'vehicle_id', 'start_date', 'end_date', 'total_price'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = [
+        'landlord_name',
+        'landlord_cpf',
+        'driver_license_number',
+        'driver_license_issue_date',
+        'birth_date',
+        'phone_1',
+        'phone_2',
+        'mother_name',
+        'father_name',
+        'start_date',
+        'end_date',
+        'cost',
+        'deposit',
+        'zip_code',
+        'state',
+        'city',
+        'neighborhood',
+        'street',
+        'house_number',
+        'complement',
+        'photo',
+        'vehicle_id'
+    ];
 
     public function vehicle()
     {
