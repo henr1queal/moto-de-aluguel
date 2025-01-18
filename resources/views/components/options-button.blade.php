1c71fd5710a19238024ea94f0e7131155f8d45a2
Henrique Silva <henriquersilva.al@gmail.com>
2025-01-18 12:06:39 -0300

    feat: componente options button adicionado

---
+++ resources/views/components/options-button.blade.php
@@ -0,0 +1,36 @@
+<div class="container text-end">
+    <div class="btn-group dropup-center dropup mt-2">
+        <button type="button" class="btn btn-light fs-1 py-0 rounded-3" data-bs-toggle="dropdown" aria-expanded="false">
+            <strong>+</strong>
+        </button>
+        <ul class="dropdown-menu p-2 text-center shadow mb-1" style="width: auto; min-width: 70px;">
+            <li>
+                <a href="#" class="d-block text-decoration-none text-black">
+                    <img src="{{ asset('assets/svg/multa.svg') }}" alt="Icon 1" class="img-fluid mx-auto" style="width: 30px; height: auto;">
+                    <small class="d-block" style="font-size: 12px;">Multas</small>
+                </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+                <a href="#" class="d-block text-decoration-none text-black">
+                    <img src="{{ asset('assets/svg/chave-inglesa.svg') }}" alt="Icon 2" class="img-fluid mx-auto" style="width: 30px; height: auto;">
+                    <small class="d-block" style="font-size: 12px;">Manut.</small>
+                </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+                <a href="#" class="d-block text-decoration-none text-black">
+                    <span class="fs-2 text-black"><strong>$</strong></span>
+                    <small class="d-block" style="font-size: 12px;">Finanças</small>
+                </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+                <a href="#" class="d-block text-decoration-none text-black">
+                    <span class="fs-2 text-black"><strong>KM</strong></span>
+                    <small class="d-block" style="font-size: 12px;">KM Diária</small>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
