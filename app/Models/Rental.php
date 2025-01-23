f54db99778fc14fb9d4decce537274620ec6670e
Henrique Silva <henriquersilva.al@gmail.com>
2025-01-22 23:28:49 -0300

    feat: coluna finished_at adicionada a locação.

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Rental extends Model
 {
@@ -32,8 +33,24 @@ class Rental extends Model
         'house_number',
         'complement',
         'photo',
-        'vehicle_id'
+        'vehicle_id',
+        'finished_at'
     ];
+
+    protected $keyType = 'string'; // Indica que a chave primária é uma string.
+    public $incrementing = false; // Desativa o autoincremento.
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Gera UUID automaticamente ao criar um novo registro
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid();
+            }
+        });
+    }
 
     public function vehicle()
     {
